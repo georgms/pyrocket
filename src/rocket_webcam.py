aa142c43c98686487ed96863e3a2a44a5c8c6022
Georg M. Sorst <g.sorst@findologic.com>
2017-03-19 12:47:19 +0100

    Add multithreading

--- src/rocket_webcam.py
+++ src/rocket_webcam.py
@@ -6,6 +6,9 @@ require('2.0')
 import gtk, gobject
 import time
 import logging
+import multiprocessing, threading
+import sys, pickle
+from inspect import currentframe, getframeinfo
 
 # ==================================
 
@@ -17,6 +20,13 @@ class WebcamManager:
 # ==================================
 
 class VideoWindow(gtk.Frame):
+	
+	display_frame = None
+	input_image_data = None
+	input_image_size = None
+	video_dimensions = [320, 240]
+	no_face_detected = [-1, -1, -1, -1]
+	last_output_update = 0
 
 	def __init__(self):
 
@@ -50,17 +60,31 @@ class VideoWindow(gtk.Frame):
 
 		self.capture = None
 
-		self.cascade = cv.Load('haarcascade_frontalface_alt.xml')
-		# create storage
-		self.storage = cv.CreateMemStorage(0)
-		
 		self.threshold = 0
 		
 		self.faces = []
-		self.previous_face = None
 
 		# remember the last movement direction to be able to consistently move in 1 direction		
 		self.last_movement = None
+		
+		self.input_pipe = multiprocessing.Pipe()
+		self.output_pipe = multiprocessing.Pipe()
+
+		self.face_detector_thread = None		
+		#self.face_detector_thread = multiprocessing.Process(target = self.detect_faces, args = (self.input_pipe[1], self.output_pipe[0]))
+		
+		# Initialize shared memory structures
+		bytes_per_pixel = 3
+		self.input_image_data = multiprocessing.Array('c', self.video_dimensions[0] * self.video_dimensions[1] * bytes_per_pixel)
+		self.input_image_size = multiprocessing.Array('i', 2)
+		
+		self.output_image_data = multiprocessing.Array('c', self.video_dimensions[0] * self.video_dimensions[1] * bytes_per_pixel)
+		self.output_image_size = multiprocessing.Array('i', 2)
+		
+		self.detected_face_position = multiprocessing.Array('i', 4)
+		self.detected_face_position[:] = self.no_face_detected[:]
+		
+		self.frame_queue = multiprocessing.Queue(1)
 		
 		logging.basicConfig(level=logging.DEBUG)
 
@@ -81,15 +105,16 @@ class VideoWindow(gtk.Frame):
 
 	def start_capture(self, device):
 
-#		video_dimensions = [176, 144]
-		video_dimensions = [320, 240]
-
-                if not self.capture:
+		if not self.capture:
 
 			self.capture = cv.CreateCameraCapture (device)
 			
-			cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_WIDTH, video_dimensions[0])
-			cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_HEIGHT, video_dimensions[1])
+			cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_WIDTH, self.video_dimensions[0])
+			cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_HEIGHT, self.video_dimensions[1])
+			
+			#self.face_detector_thread = multiprocessing.Process(target = self.detect_faces, args = (self.input_image_data, self.input_image_size, self.detected_face_position))
+			self.face_detector_thread = multiprocessing.Process(target = self.detect_faces, args = (self.frame_queue, self.detected_face_position))
+			self.face_detector_thread.start()
 
 	# -----------------------------------
 
@@ -98,6 +123,8 @@ class VideoWindow(gtk.Frame):
                 	del(self.capture)
 
 		self.capture = None
+		if self.face_detector_thread != None:
+			self.face_detector_thread.stop = True
 
 	# -----------------------------------
 
@@ -134,43 +161,40 @@ class VideoWindow(gtk.Frame):
 	# -------------------------------------------
 
 	def run(self):
-
+		
 		if self.capture:
-			webcam_frame = cv.QueryFrame( self.capture )
+			begin_grab = time.time()
+			cv.GrabFrame(self.capture)
+			end_grab = time.time()
+			begin_retrieve = time.time()
+			webcam_frame = cv.RetrieveFrame( self.capture )
+			end_retrieve = time.time()
+			#logging.debug("Grab = %ss, Retrieve = %ss" , end_grab - begin_grab, end_retrieve - begin_retrieve)
 		else:
 			print "Capture failed!"
 			return
-
+		
 		if self.inverted_video.get_active():
 			cv.ConvertImage(webcam_frame, webcam_frame, cv.CV_CVTIMG_FLIP)
 		cv.ConvertImage(webcam_frame, self.display_frame, cv.CV_CVTIMG_SWAP_RB)
-
-		faces = self.detect_faces(self.display_frame, self.previous_face)
 		
-		# See if the face count changed, if so display a message
-		if len(self.faces) != len(faces):
-			if len(faces) == 0:
-				logging.info("No faces found")
-			else:
-				logging.info("Found %s faces", len(faces))
-				
-		self.previous_face = None
+		pimg = self.display_frame.tostring()
+		pimg2 = pickle.dumps(pimg,-1)
+		self.frame_queue.put([pimg2,cv.GetSize(self.display_frame)])
+
+		if self.detected_face_position[:] != self.no_face_detected[:]:
+			#logging.debug("Found face: [%s, %s, %s, %s]", self.detected_face_position[0], self.detected_face_position[1], self.detected_face_position[2], self.detected_face_position[3])
 			
-		if len(faces) == 1:
-			
-			# faces[0] is the first detected face, faces[0][0] is the position tuple of the first detected face
-			face = faces[0][0]
-			self.previous_face = face
+			face = self.detected_face_position
 			self.mark_face(face, self.display_frame)
 			
 			center = (self.display_frame.width / 2, self.display_frame.height / 2)
 			target = (face[0] + face[2] / 2, face[1] + face[3])
 			self.center_target(center, target)
 		else:
+			#logging.debug("No face found")
 			self.rocket_frontend.movement_wrapper(5)
-			
-		self.faces = faces
-				
+		
 		incoming_pixbuf = gtk.gdk.pixbuf_new_from_data(
 				self.display_frame.tostring(),
 				gtk.gdk.COLORSPACE_RGB,
@@ -180,58 +204,91 @@ class VideoWindow(gtk.Frame):
 				self.display_frame.height,
 				self.display_frame.width * 3)
 		incoming_pixbuf.copy_area(0, 0, self.display_frame.width, self.display_frame.height, self.webcam_pixbuf, 0, 0)
-
+		
+		this_update = time.time()
+		time_since_last_update = this_update - self.last_output_update
+		fps = 1 / time_since_last_update 
+		self.last_output_update = this_update
+		logging.debug("%s FPS", fps)
+		
 		self.video_image.queue_draw()
-
 
 		return self.video_enabled_button.get_active()
 	
-	def detect_faces(self, input_image, previous_face = None):
+	#def detect_faces(self, input, output):
+	#def detect_faces(self, input_image_data, input_image_size, detected_face_position):
+	def detect_faces(self, frame_queue, detected_face_position):
 		""" Detect a face in the given input_image, optionally provide a previous_face to speed up detection """
 		
-		offset_x = 0
-		offset_y = 0
+		cascade = cv.Load('haarcascade_frontalface_alt.xml')
+		storage = cv.CreateMemStorage(0)
 		
-		scale_factor = 1.2
-		min_neighbors = 2
+		previous_face = None
 		
-		if previous_face != None:
+		while True:
 			
-			logging.debug("Using previous face as basis for detection")
-
-			# extract a part of the original image that surrounds the previous face			
-			area_of_interest = self.find_area_of_interest(previous_face, (input_image.width, input_image.height))
-			input_image = cv.GetSubRect(input_image, area_of_interest)
+			from_queue = frame_queue.get()
+			pimg = pickle.loads(from_queue[0])
+			input_image = cv.CreateImageHeader(from_queue[1], cv.IPL_DEPTH_8U, 3)
+			cv.SetData(input_image, pimg)
 			
-			# the coordinates of the detected faces need to offset as the detection is based on a sub-section of the original image			
-			offset_x = area_of_interest[0]
-			offset_y = area_of_interest[1]
-
-		cv.ShowImage('focus', input_image)
-		
-		begin_face_detection = int(round(time.time() * 1000))
-		faces = cv.HaarDetectObjects(input_image, self.cascade, self.storage, scale_factor, min_neighbors, cv.CV_HAAR_DO_CANNY_PRUNING)
-		end_face_detection = int(round(time.time() * 1000))
-		
-		logging.debug('face detection took %s ms', end_face_detection - begin_face_detection)
-
-		# Offset the detected faces if necessary
-		if offset_x > 0 or offset_y > 0:
-					
-			offset_faces = []
+			offset_x = 0
+			offset_y = 0
 			
-			for face in faces:
-				position = face[0]
-				neighbors = face[1]
-				offset_position = (position[0] + offset_x, position[1] + offset_y, position[2], position[3])
+			scale_factor = 1.2
+			min_neighbors = 2
+			
+			if previous_face != None:
 				
-				offset_face = (offset_position, neighbors)
-				offset_faces.append(offset_face)
+				print "Using previous face as basis for detection"
+				sys.stdout.flush()
+	
+				# extract a part of the original image that surrounds the previous face			
+				area_of_interest = self.find_area_of_interest(previous_face, (input_image.width, input_image.height))
+				input_image = cv.GetSubRect(input_image, area_of_interest)
+				
+				# the coordinates of the detected faces need to offset as the detection is based on a sub-section of the original image			
+				offset_x = area_of_interest[0]
+				offset_y = area_of_interest[1]
+	
+			cv.ShowImage('focus', input_image)
 			
-			faces = offset_faces
-		
-		return faces
-
+			begin_face_detection = int(round(time.time() * 1000))
+			faces = cv.HaarDetectObjects(input_image, cascade, storage, scale_factor, min_neighbors, cv.CV_HAAR_DO_CANNY_PRUNING)
+			end_face_detection = int(round(time.time() * 1000))
+			
+			print 'face detection took %s ms', end_face_detection - begin_face_detection
+			sys.stdout.flush()
+	
+			# Offset the detected faces if necessary
+			if offset_x > 0 or offset_y > 0:
+						
+				offset_faces = []
+				
+				for face in faces:
+					position = face[0]
+					neighbors = face[1]
+					offset_position = (position[0] + offset_x, position[1] + offset_y, position[2], position[3])
+					
+					offset_face = (offset_position, neighbors)
+					offset_faces.append(offset_face)
+				
+				faces = offset_faces
+				
+			if len(faces) == 1:
+				print "Found one face: %s", faces[0]
+				sys.stdout.flush()
+				
+				print faces[0][0]
+				detected_face_position[:] = faces[0][0]
+				previous_face = detected_face_position[:]
+			else:
+				print "faces detected", len(faces)
+				sys.stdout.flush()
+				
+				detected_face_position[:] = self.no_face_detected[:]
+				previous_face = None
+			
 	def find_area_of_interest(self, previous_face, max_dimensions):
 		""" Find the area of interest by calculating a rectangle around a previously detected face """
 		
@@ -263,37 +320,38 @@ class VideoWindow(gtk.Frame):
 	def mark_face(self, face, inputImage):
 		""" Mark the given face in the given inputImage """
 		
-		left = face[0]
-		top = face[1]
-		right = left + face[2]
-		bottom = top + face[3]
-		
-		# Mark the face
-		color = (0, 255, 0)
-		thickness = 1
-		lineType = 8
-		shift = 0
-		cv.Rectangle(inputImage, (left, top), (right, bottom), color, thickness, lineType, shift)
-		
-		centerX = right - (right - left) / 2
-		centerY = bottom - (bottom - top) / 2
-		
-		# Mark the center of the face
-		radius = 5 
-		cv.Circle(inputImage, (centerX, centerY), radius, color, thickness, lineType, shift)
-		
-		cv.Rectangle(inputImage,
-			(left, top),
-			(right, bottom),
-			(0, 255, 0),
-			3,
-			8,
-			0)
-		
-		(diff_x, diff_y) = self.get_diff_vector((centerX, centerY), (self.display_frame.width / 2, self.display_frame.height / 2))
-		
-		# Draw a line from the center of the face to the center of the image as an indication how to move the launcher
-		cv.Line(inputImage, (centerX, centerY), (inputImage.width / 2, inputImage.height / 2), color, thickness, lineType, shift)
+		if face != self.no_face_detected:
+			left = face[0]
+			top = face[1]
+			right = left + face[2]
+			bottom = top + face[3]
+			
+			# Mark the face
+			color = (0, 255, 0)
+			thickness = 1
+			lineType = 8
+			shift = 0
+			cv.Rectangle(inputImage, (left, top), (right, bottom), color, thickness, lineType, shift)
+			
+			centerX = right - (right - left) / 2
+			centerY = bottom - (bottom - top) / 2
+			
+			# Mark the center of the face
+			radius = 5 
+			cv.Circle(inputImage, (centerX, centerY), radius, color, thickness, lineType, shift)
+			
+			cv.Rectangle(inputImage,
+				(left, top),
+				(right, bottom),
+				(0, 255, 0),
+				3,
+				8,
+				0)
+			
+			(diff_x, diff_y) = self.get_diff_vector((centerX, centerY), (self.display_frame.width / 2, self.display_frame.height / 2))
+			
+			# Draw a line from the center of the face to the center of the image as an indication how to move the launcher
+			cv.Line(inputImage, (centerX, centerY), (inputImage.width / 2, inputImage.height / 2), color, thickness, lineType, shift)
 		
 	def get_diff_vector(self, point1, point2):
 		""" Get the vector between two points """
@@ -317,27 +375,32 @@ class VideoWindow(gtk.Frame):
 		logging.debug('Diff from target to center is %s, last movement was %s', diff, self.last_movement)
 		
 		# move only when the diff is bigger than this to avoid jumping around the center
-		movement_threshold = 25
+		movement_threshold = 5
 		
 		if self.rocket_frontend:
 			
 			if (self.last_movement == None or self.last_movement == LEFT) and diff[0] < -movement_threshold:
-				self.rocket_frontend.movement_wrapper(LEFT)
+				if self.last_movement != LEFT:
+					self.rocket_frontend.movement_wrapper(LEFT)
 				self.last_movement = LEFT
 				logging.debug("Moving left")
 			elif (self.last_movement == None or self.last_movement == RIGHT) and diff[0] > movement_threshold:
-				self.rocket_frontend.movement_wrapper(RIGHT)
+				if self.last_movement != RIGHT:
+					self.rocket_frontend.movement_wrapper(RIGHT)
 				self.last_movement = RIGHT
 				logging.debug("Moving right")
 			elif (self.last_movement == None or self.last_movement == DOWN) and diff[1] > movement_threshold:
-				self.rocket_frontend.movement_wrapper(DOWN)
+				if self.last_movement != DOWN:
+					self.rocket_frontend.movement_wrapper(DOWN)
 				self.last_movement = DOWN
 				logging.debug("Moving down")
 			elif (self.last_movement == None or self.last_movement == UP) and diff[1] < -movement_threshold:
-				self.rocket_frontend.movement_wrapper(UP)
+				if self.last_movement != UP:
+					self.rocket_frontend.movement_wrapper(UP)
 				self.last_movement = UP
 				logging.debug("Moving up")
 			elif self.last_movement != None:
 				self.rocket_frontend.movement_wrapper(STOP)
 				self.last_movement = None
 				logging.debug("Stopping all engines")
+				
